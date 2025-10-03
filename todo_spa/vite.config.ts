83439016a6a7fa39b2430ec7d281a2a019f45fae
Vladislav <67109556+wladek17@users.noreply.github.com>
2025-10-03 19:53:13 +0300

    Update base path

--- todo_spa/vite.config.ts
+++ todo_spa/vite.config.ts
@@ -1,19 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, ConfigEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  base: '/TodoApp/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }: ConfigEnv) => {
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    base: mode === "production" ? "/TodoApp/" : "/",
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+  };
 })
